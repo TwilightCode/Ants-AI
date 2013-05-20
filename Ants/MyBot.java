398b152577218c544f866efea51983c77ba88165
Sarga <lauri.suomi@helsinki.fi>
2013-05-20 20:19:43 +0300

    Modified AI to pick a random movement direction

--- Ants/MyBot.java
+++ Ants/MyBot.java
@@ -1,34 +1,74 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Starter bot implementation.
+ * Some methods in this class have been added or modified. 
  */
 public class MyBot extends Bot {
-    /**
-     * Main method executed by the game engine for starting the bot.
-     * 
-     * @param args command line arguments
-     * 
-     * @throws IOException if an I/O error occurs
-     */
-    public static void main(String[] args) throws IOException {
-        new MyBot().readSystemInput();
-    }
-    
-    /**
-     * For every ant check every direction in fixed order (N, E, S, W) and move it if the tile is
-     * passable.
-     */
-    @Override
-    public void doTurn() {
-        Ants ants = getAnts();
-        for (Tile myAnt : ants.getMyAnts()) {
-            for (Aim direction : Aim.values()) {
-                if (ants.getIlk(myAnt, direction).isPassable()) {
-                    ants.issueOrder(myAnt, direction);
-                    break;
-                }
-            }
-        }
-    }
+	/**
+	 * Main method executed by the game engine for starting the bot.
+	 * 
+	 * @param args
+	 *            command line arguments
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	public static void main(String[] args) throws IOException {
+		new MyBot().readSystemInput();
+	}
+
+	/**
+	 * This method has been modified Method checks valid directions, and gives
+	 * each ant valid direction to move.
+	 */
+	@Override
+	public void doTurn() {
+		Ants ants = getAnts();
+		for (Tile myAnt : ants.getMyAnts()) {
+			ArrayList<Aim> table = movement(ants, myAnt);
+			if (!table.isEmpty()) {
+				Aim direction;
+				if (table.size() == 1) {
+					direction = table.get(0);
+				} else {
+					direction = getRandomDirection(table);
+				}
+				ants.issueOrder(myAnt, direction);
+			}
+		}
+	}
+
+	/**
+	 * This method is new Method checks all possible directions and returns
+	 * arraylist containing all valid directions
+	 */
+	private ArrayList<Aim> movement(Ants ants, Tile myAnt) {
+		ArrayList<Aim> table = new ArrayList<Aim>();
+		if (ants.getIlk(myAnt, Aim.NORTH).isPassable()) {
+			table.add(Aim.NORTH);
+		}
+		if (ants.getIlk(myAnt, Aim.EAST).isPassable()) {
+			table.add(Aim.EAST);
+		}
+		if (ants.getIlk(myAnt, Aim.SOUTH).isPassable()) {
+			table.add(Aim.SOUTH);
+		}
+		if (ants.getIlk(myAnt, Aim.WEST).isPassable()) {
+			table.add(Aim.WEST);
+		}
+		return table;
+	}
+
+	/**
+	 * This method is new Method picks random direction from the given arraylist
+	 * and returns it.
+	 */
+
+	private Aim getRandomDirection(ArrayList<Aim> table) {
+		Random rand = new Random();
+		return table.get(rand.nextInt(table.size()));
+	}
 }
