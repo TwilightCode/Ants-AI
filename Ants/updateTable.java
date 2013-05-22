c5f279fccb8cc8c675218d73f3b19598ca52d610
Sarga <lauri.suomi@helsinki.fi>
2013-05-22 20:50:53 +0300

    fixed problems that appeared during the testing

--- Ants/updateTable.java
+++ Ants/updateTable.java
@@ -1,8 +1,8 @@
 public class updateTable {
-	public static int[][] NorthTable;
-	public static int[][] EastTable;
-	public static int[][] SouthTable;
-	public static int[][] WestTable;
+	public  int[][] NorthTable;
+	public  int[][] EastTable;
+	public  int[][] SouthTable;
+	public  int[][] WestTable;
 
 	/**
 	 * 
@@ -63,32 +63,33 @@ public class updateTable {
 	 *            value of ants range of vision in a square format.
 	 */
 	public void setNorthTable(int vision) {
-		int k = 0;
-		int i = 0;
+		int k = -1;
+		int i = -1;
 		int value = 0;
-		int range = (int) Math.log(vision);
+		int range = (int) Math.sqrt(vision);
 		NorthTable = new int[2 * range + 1][2];
 		for (int j = range; j > 0; j--) {
 			while (value < vision) {
-				value = (int) Math.pow(j, 2) + (int) Math.pow(i, 2);
 				i++;
-			}
-			NorthTable[k][0] = i;
-			NorthTable[k][1] = j;
+				value = (int) Math.pow(j, 2) + (int) Math.pow(i, 2);
+				}
 			k++;
+			NorthTable[k][0] = j;
+			NorthTable[k][1] = i;
 			i = 0;
+			value = 0;
 		}
-		i = k;
-		NorthTable[i][0] = range + 1;
-		NorthTable[i][1] = 0;
-		for (k += -1; k > 0; k--) {
+		i = range;
+		NorthTable[k+1][0] = 0;
+		NorthTable[k+1][1] = range + 1;
+		for (;k >= 0; k--) {
 			i++;
 			NorthTable[i][0] = -1 * NorthTable[k][0];
 			NorthTable[i][1] = NorthTable[k][1];
 		}
-		setEastTable();
-		setSouthTable();
-		setWestTable();
+		//setEastTable();
+		//setSouthTable();
+		//setWestTable();
 	}
 
 	/**
