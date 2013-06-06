c34513a15cb4c2af863229646b745197f4420147
Sarga <lauri.suomi@helsinki.fi>
2013-06-06 22:30:15 +0300

    added some tests and fixed old ones

--- tests/UnchartedTest.java
+++ tests/UnchartedTest.java
@@ -1,13 +1,15 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class UnchartedTest {
-	gameMap Map;
-	unchartedTable uncharted;
-	
 
+	unchartedTable uncharted;
+	Ants game;
+	private int[][] testTable;
 
 	int[][] comparisonTable = { { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 }, { 30, 30, 30, 30, 30, 30, 30, 30, -1, 30 },
 			{ 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 }, { 30, 30, 30, -1, 30, 30, 30, 30, 30, 30 }, { 30, 30, 30, -1, 30, 30, 30, 30, 30, 30 },
@@ -23,24 +25,30 @@ public class UnchartedTest {
 	public void setUpTests() {
 		Ants game = new Ants(100, 100, 10, 10, 10, 55, 25, 55);
 		updateTable update = new updateTable(6);
-		this.Map = new gameMap(game, update); 
-		uncharted = new unchartedTable(update, Map);
+		uncharted = new unchartedTable(update, game);
+		testTable = uncharted.getUncharted();
 	}
 
 	@Test
 	public void initilalizeUncharted() {
-		int[][] testTable = uncharted.getUncharted();
-		for (int i = 0; i < testTable.length; i++) {
-			for (int j = 0; j < testTable[0].length; j++) {
+		for (int i = 0; i < comparisonTable.length; i++) {
+			for (int j = 0; j < comparisonTable[0].length; j++) {
 				assertEquals(31, testTable[i][j]);
 			}
 		}
 	}
 
 	@Test
+	public void markAsSeen() {
+		Tile tile = new Tile(1, 1);
+		uncharted.setToSeen(tile);
+		assertEquals(0, testTable[1][1]);
+	}
+
+	@Test
 	public void createViewArea() {
-		uncharted.setViewArea(1, 1);
-		int[][] testTable = uncharted.getUncharted();
+		Tile tile = new Tile(1, 1);
+		uncharted.setViewArea(tile);
 		for (int i = 0; i < comparisonTable2.length; i++) {
 			for (int j = 0; j < comparisonTable2[0].length; j++) {
 				assertEquals(comparisonTable2[i][j], testTable[i][j]);
@@ -50,37 +58,64 @@ public class UnchartedTest {
 
 	@Test
 	public void valueOverTopBorder() {
-		assertEquals(0, Map.isHeightOverBoard(10));
-		assertEquals(0, Map.isWidthOverBoard(10));
+		assertEquals(0, uncharted.isOverBoard(10, 10));
 	}
 
 	@Test
 	public void valueOverBottomBorder() {
-		assertEquals(9, Map.isHeightOverBoard(-1));
-		assertEquals(9, Map.isWidthOverBoard(-1));
+		assertEquals(9, uncharted.isOverBoard(-1, 10));
 	}
 
 	@Test
 	public void valueIsWithinBorders() {
-		assertEquals(5, Map.isHeightOverBoard(5));
-		assertEquals(5, Map.isWidthOverBoard(5));
+		assertEquals(5, uncharted.isOverBoard(5, 10));
 	}
 
 	@Test
 	public void getsRightMovementValue() {
-		assertEquals(155, uncharted.getMoveValue(1, 1, Aim.NORTH));
+		Tile tile = new Tile(1, 1);
+		assertEquals(155, uncharted.getMoveValue(tile, Aim.NORTH));
 	}
-	
+
+	@Test
+	public void setCoordinates() {
+		Tile tile = new Tile(1, 1);
+		tile = uncharted.setCoordinates(tile, 3, 5);
+		assertEquals(6, tile.getRow());
+		assertEquals(4, tile.getCol());
+	}
+
 	@Test
 	public void moveViewArea() {
-		int[][] testTable = uncharted.getUncharted();
-		uncharted.setTable(6, 3, Aim.NORTH);
-		int table[][] = { { 31, 31, 0, 0, 0, 31, 31 }, { 31, 0, 31, 31, 31, 0, 31 }, { 31, 31, 31, 31, 31, 31, 31 }, { 31, 31, 31, 31, 31, 31, 31 } };
+		Tile tile = new Tile(3, 6);
+		uncharted.updateUnknown(tile, Aim.NORTH);
+		int table[][] = { { 31, 31, 31, 31, 31, 0, 0, 0, 31, 31 }, { 31, 31, 31, 31, 0, 31, 31, 31, 0, 31 }, { 31, 31, 31, 31, 31, 31, 31, 31, 31, 31 },
+				{ 31, 31, 31, 31, 31, 31, 31, 31, 31, 31 } };
 		for (int i = 0; i < table.length; i++) {
 			for (int j = 0; j < table[0].length; j++) {
-				assertEquals(table[i][j], testTable[i][j + 3]);
+				assertEquals(table[i][j], testTable[i][j]);
 			}
-
 		}
 	}
+// doesn't work unless you disable methods check if the tile can be seen
+/*	@Test
+	public void raiseUncharted() {
+		Tile tile;
+		for (int i = 0; i < comparisonTable2.length; i++) {
+			for (int j = 0; j < comparisonTable2[0].length; j++) {
+				tile = new Tile(i, j);
+				uncharted.setToSeen(tile);
+			}
+		}
+		uncharted.raiseUncharted();
+		for (int i = 0; i < comparisonTable2.length; i++) {
+			for (int j = 0; j < comparisonTable2[0].length; j++) {
+				assertEquals(5, testTable[i][j]);
+			}
+			}
+	}
+	*/
+	
+
+	
 }
