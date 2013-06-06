d62fd6dd5922f73fa55f0f6ce91a2cd0f550ccef
Sarga <lauri.suomi@helsinki.fi>
2013-06-06 22:22:34 +0300

    Made changes so that class communicates better with the game

--- Ants/unchartedTable.java
+++ Ants/unchartedTable.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class unchartedTable {
 	private int[][] uncharted;
 	updateTable update;
-	gameMap area;
-	int mapHeight;
-	int mapWidth;
+	Ants game;
+	private int mapHeight;
+	private int mapWidth;
 
 	/**
 	 * constructor
@@ -13,13 +16,12 @@ public class unchartedTable {
 	 * @param gameField
 	 *            the game table
 	 */
-	public unchartedTable(updateTable update, gameMap map) {
+	public unchartedTable(updateTable update, Ants game) {
+		this.game = game;
 		this.update = update;
-		area = map;
-		Ilk[][] field = area.getMap();
-		initializeUncharted(field);
-		mapHeight = field.length;
-		mapWidth = field[0].length;
+		this.mapHeight = game.getRows();
+		this.mapWidth = game.getCols();
+		initializeUncharted();
 	}
 
 	/**
@@ -36,13 +38,26 @@ public class unchartedTable {
 	 * @param gameField
 	 *            is the map of current game
 	 */
-	public void initializeUncharted(Ilk[][] gameField) {
-		uncharted = new int[gameField.length][gameField[0].length];
-		for (int i = 0; i < gameField.length; i++) {
-			for (int j = 0; j < gameField[0].length; j++) {
+	public void initializeUncharted() {
+		uncharted = new int[mapHeight][mapWidth];
+		for (int i = 0; i < mapHeight; i++) {
+			for (int j = 0; j < mapWidth; j++) {
 				uncharted[i][j] = 31;
 			}
 		}
+	}
+
+	public void setUnknownTile(Tile tile) {
+		Ilk type = game.getIlk(tile);
+		if (type == Ilk.WATER) {
+			uncharted[tile.getRow()][tile.getCol()] = -1;
+		} else {
+			uncharted[tile.getRow()][tile.getCol()] = 0;
+		}
+	}
+
+	public void setToSeen(Tile tile) {
+		uncharted[tile.getRow()][tile.getCol()] = 0;
 	}
 
 	/**
@@ -53,20 +68,17 @@ public class unchartedTable {
 	 * @param col
 	 *            y line
 	 */
-	public void setViewArea(int row, int col) {
+	public void setViewArea(Tile tile) {
 		int[][] easternLine = update.getEastTable();
 		int[][] westernLine = update.getWestTable();
-		int width = 0;
-		int height = 0;
+		Tile newTile;
 		for (int i = 0; i < westernLine.length; i++) {
-			height = westernLine[i][1] + col;
 			for (int j = westernLine[i][0] + 1; j < easternLine[i][0]; j++) {
-				width = j + row;
-				width = area.isWidthOverBoard(width);
-				height = area.isHeightOverBoard(height);
-				if (uncharted[height][width] != -1) {
-					uncharted[height][width] = 0;
-					area.setMapTile(height, width);
+				newTile = setCoordinates(tile, j, westernLine[i][1]);
+				if (uncharted[newTile.getRow()][newTile.getCol()] == 31) {
+					setUnknownTile(newTile);
+				} else {
+					setToSeen(newTile);
 				}
 			}
 		}
@@ -80,13 +92,48 @@ public class unchartedTable {
 	 *            contains the current game
 	 */
 	public void raiseUncharted() {
+		Tile tile;
 		for (int i = 0; i < uncharted.length; i++) {
 			for (int j = 0; j < uncharted[0].length; j++) {
-				if ((uncharted[i][j] < 30 && uncharted[i][j] > -1 && !area.isSeen(i,j))) {
-					uncharted[i][j] += 1;
+				tile = new Tile(i, j);
+				if (((uncharted[i][j] + 5) < 30 && uncharted[i][j] > -1 && game.isVisible(tile))) {
+					uncharted[i][j] += 5;
 				}
 			}
 		}
+	}
+
+	public Aim getBestDirection(Tile tile) {
+		int value;
+		int max = 0;
+		Aim direction = null;
+		ArrayList<Aim> directions = getMovableDirections(tile);
+		if (directions.size() == 0) {
+			return null;
+		}
+		for (int i = 0; i < directions.size(); i++) {
+			value = getMoveValue(tile, directions.get(i));
+			if (max < value) {
+				max = value;
+				direction = directions.get(i);
+			} else if (max == value) {
+				direction = getRandomDirection(direction, directions.get(i));
+			}
+		}
+		return direction;
+	}
+
+	public Aim getRandomDirection(Aim direction, Aim otherDirection) {
+		Random rand = new Random();
+		if (rand.nextInt(2) == 0) {
+			return direction;
+		} else {
+			return otherDirection;
+		}
+	}
+
+	public Tile setCoordinates(Tile tile, int borderX, int borderY) {
+		return new Tile(isOverBoard((tile.getRow() + borderY), mapHeight), isOverBoard((tile.getCol() + borderX), mapWidth));
 	}
 
 	/**
@@ -101,19 +148,46 @@ public class unchartedTable {
 	 *            where ant would move
 	 * @return value is the amount gain from moving to this direction
 	 */
-	public int getMoveValue(int row, int col, Aim direction) {
-		int width = 0;
-		int height = 0;
+	public int getMoveValue(Tile tile, Aim direction) {
+		Tile newTile;
 		int value = 0;
 		int[][] table = update.getUpdateTable(direction);
 		for (int i = 0; i < table.length; i++) {
-			width = row + table[i][0];
-			height = col + table[i][1];
-			width = area.isWidthOverBoard(width);
-			height = area.isHeightOverBoard(height);
-			value += uncharted[height][width];
+			newTile = setCoordinates(tile, table[i][0], table[i][1]);
+			value += uncharted[newTile.getRow()][newTile.getCol()];
 		}
 		return value;
+	}
+
+	public boolean isTileBlocked(Tile tile) {
+		if (game.getIlk(tile) == Ilk.LAND || game.getIlk(tile) == Ilk.DEAD) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public ArrayList<Aim> getMovableDirections(Tile tile) {
+		Tile newTile;
+		ArrayList<Aim> table = new ArrayList<Aim>();
+		Aim[] directions = { Aim.NORTH, Aim.EAST, Aim.SOUTH, Aim.WEST };
+		for (int i = 0; i < directions.length; i++) {
+			newTile = game.getTile(tile, directions[i]);
+			if (!isTileBlocked(newTile)) {
+				table.add(directions[i]);
+			}
+		}
+		return table;
+	}
+
+	public int isOverBoard(int value, int max) {
+		if (value >= max) {
+			return value - max;
+		} else if (value < 0) {
+			return max + value;
+		} else {
+			return value;
+		}
 	}
 
 	/**
@@ -126,19 +200,17 @@ public class unchartedTable {
 	 * @param direction
 	 *            where ant moves
 	 */
-	public void setTable(int row, int col, Aim direction) {
-		int width = 0;
-		int height = 0;
+		public void updateUnknown(Tile tile, Aim direction) {
+		Tile newTile;
 		int[][] table = update.getUpdateTable(direction);
 		for (int i = 0; i < table.length; i++) {
-			width = row + table[i][0];
-			height = col + table[i][1];
-			width = area.isWidthOverBoard(width);
-			height = area.isHeightOverBoard(height);
-			if (uncharted[height][width] != -1) {
-				uncharted[height][width] = 0;
-				area.setMapTile(height, width);
+			newTile = setCoordinates(tile, table[i][0], table[i][1]);
+			if (uncharted[newTile.getRow()][newTile.getCol()] == 31) {
+				setUnknownTile(newTile);
+			} else {
+				setToSeen(newTile);
 			}
 		}
 	}
+		
 }
